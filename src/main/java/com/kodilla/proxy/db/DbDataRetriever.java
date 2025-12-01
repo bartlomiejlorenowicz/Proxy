05512bfb3b5cfa1eb87a82f071052fcf83d50a58
Bartlomiej <bartlomiejlorenowicz@gmail.com>
2025-12-02 00:06:55 +0100

    Initialize new Spring Boot application with Gradle build system. Includes project structure, proxy design pattern implementation, and `.gitignore` setup.

---
+++ src/main/java/com/kodilla/proxy/db/DbDataRetriever.java
@@ -0,0 +1,8 @@
+package com.kodilla.proxy.db;
+
+public interface DbDataRetriever {
+
+    int getFirstValue() throws InterruptedException;
+    int getSecondValue() throws InterruptedException;
+    int getThirdValue() throws InterruptedException;
+}
---
+++ src/test/java/com/kodilla/proxy/db/KodillaProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.proxy.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
